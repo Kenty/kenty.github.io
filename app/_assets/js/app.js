6c333ee8e0b99aba70185e2f0b0d12635950d8ef
kenty <kent6223@gmail.com>
2015-10-19 15:28:06 +0900

    Added tags pages, js smooth-scroll and fixed them related to files

--- app/_assets/js/app.js
+++ app/_assets/js/app.js
@@ -1,4 +1,6 @@
 var $ = require('jquery');
+// var smoothScroll = require('smooth-scroll');
+var smoothScroll = require('smooth-scroll');
 // require('modernizr');
 // require('foundation');
 require('responsive-nav');
@@ -6,28 +8,10 @@ require('fastClick');
 require('./my_sns');
 require('./my_webfont');
 require('./accessibility');
+require('./my_init');
 
 (function() {
   'use strict';
-
-  if ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
-
-    var doc = document,
-      win = window,
-      $ = doc.querySelectorAll.bind(doc);
-
-    // init Responsive Nav
-    var nav = responsiveNav('#navigation', {
-      customToggle: '#nav-toggle',
-      openPos: 'absolute',
-      transition: 300
-        // navClass: 'nav-collapse'
-    });
-
-    win.addEventListener('load', function() {
-      FastClick.attach(doc.body);
-    }, false);
-  }
 
   console.log('jQuery, Modernizr, foundation and responsive loaded');
 
---
+++ app/_assets/js/my_init.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  if ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+
+    var doc = document,
+      win = window,
+      $ = doc.querySelectorAll.bind(doc),
+      ss = smoothScroll;
+
+
+    // init Responsive Nav
+    var nav = responsiveNav('#navigation', {
+      customToggle: '#nav-toggle',
+      openPos: 'absolute',
+      transition: 300
+        // navClass: 'nav-collapse'
+    });
+
+    // init smooth scrolling
+    ss.init({
+      speed: 500,
+      easing: 'easeInOutQuad',
+      offset: 80,
+    });
+
+    if (win.location.hash) {
+      var options = {
+        speed: 500,
+        easing: 'easeInOutCubic',
+        offset: 30,
+      };
+      ss.animateScroll( null, win.location.hash, options);
+    }
+
+    win.addEventListener('load', function() {
+      FastClick.attach(doc.body);
+    }, false);
+
+    doc.querySelector('#page').addEventListener('touchend', function(event) {
+      event.preventDefault();
+      // event.stopPropagation();
+    }, false);
+
+  }
+
+})();
