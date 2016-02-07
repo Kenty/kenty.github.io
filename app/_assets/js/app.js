1997cdb1752cf617e7743f66df3bfcf4548d1e0f
kenty <kent6223@gmail.com>
2016-02-07 18:34:10 +0900

    Updated

--- app/_assets/js/app.js
+++ app/_assets/js/app.js
@@ -1,18 +1,13 @@
 var $ = require('jquery');
-// var smoothScroll = require('smooth-scroll');
-var smoothScroll = require('smooth-scroll');
-// require('modernizr');
-// require('foundation');
-require('responsive-nav');
-require('fastClick');
+
 require('./my_sns');
 require('./my_webfont');
 require('./accessibility');
 require('./my_init');
 
-(function() {
-  'use strict';
+// (function() {
+//   'use strict';
 
-  console.log('jQuery, Modernizr, foundation and responsive loaded');
+//   console.log('jQuery, Modernizr, foundation and responsive loaded');
 
-})();
+// })();
